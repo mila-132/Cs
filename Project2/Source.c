530200c757f89cb0b375c3295a26570400e7f334
mila-132 <malahatjafarli92@gmail.com>
2024-11-12 13:47:53 +0900

    Add files via upload

---
+++ Project2/Source.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main() {
+	int score;
+	scanf("%d", &score);
+	if (score <= 100 && score >= 90) {
+		printf("A");
+	}
+	else if (score <= 89 && score >= 80) {
+		printf("B");
+	}
+	else if (score <= 79 && score >= 70) {
+		printf("C");
+	}
+	else if (score <= 69 && score >= 60) {
+		printf("D");
+	}
+	else if (score < 60 && score >= 0) {
+		printf("F");
+	}
+	return 0;
+
+	}
