b4d600bd7b3fa6be21ef542549ee357ab3805af1
mila-132 <malahatjafarli92@gmail.com>
2024-11-12 14:30:05 +0900

    Add files via upload

---
+++ Project25314/Source.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int N;
+	scanf("%d", &N);
+	N = N / 4;
+	for (int i = 0; i<N ; i++) {
+		
+		printf("long ");
+			
+
+	}
+	printf("int");
+
+	return 0;
+}
